383e949b355a5c23db4676b9fa63fbfc35bce7bb
Kevin Hogan <kevin.hogan1992@gmail.com>
2018-11-25 10:55:06 -0500

    Builder for train config

---
+++ config.py
@@ -0,0 +1,40 @@
+import torch.nn as nn
+
+class TrainConfig():
+    def set_data_folder(self, data_folder):
+        self.data_folder = data_folder
+        return self
+
+    def set_num_threads(self, num_threads):
+        self.num_threads = num_threads
+        return self
+
+    def set_learning_rate(self, learning_rate):
+        self.learning_rate = learning_rate
+        return self
+
+    def set_batch_size(self, batch_size):
+        self.batch_size = batch_size
+        return self
+
+    def set_dropouts(self, dropouts):
+        self.dropouts = dropouts
+        return self
+    
+    def set_num_epochs(self, num_epochs):
+        self.num_epochs = num_epochs
+        return self
+    
+    def set_test_to_all_ratio(self, test_to_all_ratio):
+        self.test_to_all_ratio = test_to_all_ratio
+        return self
+
+    def set_results_dir(self, results_dir):
+        self.results_dir = results_dir
+        return self
+
+class Dropouts():
+    def __init__(self, input_dropout, conv_dropout, lstm_dropout):
+        self.input_dropout = nn.Dropout(input_dropout)
+        self.conv_dropout = nn.Dropout(conv_dropout)
+        self.lstm_dropout = nn.Dropout(lstm_dropout)
--- train.py
+++ train.py
@@ -12,6 +12,7 @@ from sklearn.preprocessing import scale
 from itertools import compress
 import argparse
 from model import CRNN, ConvNet
+from config import TrainConfig, Dropouts
 
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -38,46 +39,46 @@ class CustomDataset(Dataset):
 
         return data, label[0]
 
-def diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_ALL_RATIO, data_folder, results_dir, nthreads=8):
+def diffraction_train(config):
 
     # initialize dataset
-    if not os.path.exists(data_folder):
+    if not os.path.exists(config.data_folder):
         print("data folder non-exist")
         return
     
-    labelpath = os.path.join(data_folder, 'labels.csv')
+    labelpath = os.path.join(config.data_folder, 'labels.csv')
     csvfile = open(labelpath, 'r')
     csv_reader = csv.reader(csvfile, delimiter=',')
     next(csv_reader, None)
     dataset = []
     for line in csv_reader:
-        npypath = os.path.join(data_folder, line[0])
+        npypath = os.path.join(config.data_folder, line[0])
         # if os.path.exists(npypath):
         dataset.append([npypath, [float(x) for x in line[1:4]]])
         # if len(dataset)>1000:
         #     break
 
-    train_data_entries, val_data_entries = train_test_split(dataset, test_size=TEST_TO_ALL_RATIO)
+    train_data_entries, val_data_entries = train_test_split(dataset, test_size=config.test_to_all_ratio)
     train_dataset = CustomDataset(train_data_entries)
     val_dataset = CustomDataset(val_data_entries)
 
     # initialize Data loader
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
-                                               batch_size=batch_size,
-                                               shuffle=True, num_workers=nthreads)
+                                               batch_size=config.batch_size,
+                                               shuffle=True, num_workers=config.num_threads)
     val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
-                                            batch_size=batch_size,
-                                            shuffle=False, num_workers=nthreads)
+                                            batch_size=config.batch_size,
+                                            shuffle=False, num_workers=config.num_threads)
 
-    model = CRNN(device, dropouts).to(device)
+    model = ConvNet(device, config.dropouts).to(device)
 
     # Loss and optimizer
     criterion = nn.MSELoss(reduction='sum')
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(model.parameters(), lr=config.learning_rate)
     # optimizer = torch.optim.Adadelta(model.parameters(), lr=learning_rate)
 
-    os.makedirs(results_dir)
-    writer = SummaryWriter(results_dir)
+    os.makedirs(config.results_dir)
+    writer = SummaryWriter(config.results_dir)
 
     # Train the model
     ts = time.time()
@@ -86,7 +87,7 @@ def diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_A
     early_stop_cnt = 0
     lowest_error = 1e6
     total_step = len(train_loader)
-    for epoch in range(num_epochs):
+    for epoch in range(config.num_epochs):
         if not early_stop_flag:
             model.train()
             for i, (images, labels) in enumerate(train_loader):
@@ -106,7 +107,7 @@ def diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_A
                 writer.add_scalar("training_loss", loss.item() / len(labels), niter)
                 if i % 50 == 0:
                     print('[Training] Epoch [{}/{}], Step [{}/{}], Loss: {:.8f}, time elapsed: {:.2f} seconds'
-                          .format(epoch + 1, num_epochs, i + 1, total_step, loss.item() / len(labels), time.time()-ts))
+                          .format(epoch + 1, config.num_epochs, i + 1, total_step, loss.item() / len(labels), time.time()-ts))
 
             # Use val set to test the model at each epoch
             model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
@@ -128,7 +129,7 @@ def diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_A
             if average_val_loss < lowest_error:
                 lowest_error = average_val_loss
                 early_stop_cnt = 0
-                torch.save(model.state_dict(), os.path.join(results_dir, "best_valid.pth"))
+                torch.save(model.state_dict(), os.path.join(config.results_dir, "best_valid.pth"))
             else:
                 early_stop_cnt += 1
                 if early_stop_cnt >= num_iterations_before_early_stop:
@@ -138,45 +139,36 @@ def diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_A
             writer.close()
             break  # early stop break
 
-class Dropouts():
-    def __init__(self, input_dropout, conv_dropout, lstm_dropout):
-        self.input_dropout = nn.Dropout(input_dropout)
-        self.conv_dropout = nn.Dropout(conv_dropout)
-        self.lstm_dropout = nn.Dropout(lstm_dropout)
-
 if __name__ == "__main__":
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
     parser = argparse.ArgumentParser(prog='diffraction_CNNtrain',
                                      description="""Script to train the InstantDiffraction system""")
-    parser.add_argument("--input", "-i", required=False, default="data", help="Directory where data and labels are", type=str)
+    parser.add_argument("--input", "-i", default="data", help="Directory where data and labels are", type=str)
     parser.add_argument("--nthreads", "-n", type=int, default=0, help="Number of threads to use")
-    parser.add_argument("--rate", "-r", type=float, default=-1, help="Choose a learning rate, default to sweep")
-    parser.add_argument("--batchsize", "-b", type=int, default=-1, help="Choose a batchsize, default to sweep")
+    parser.add_argument("--rate", "-r", type=float, default=None, help="Choose a learning rate, default to sweep")
+    parser.add_argument("--batchsize", "-b", type=int, default=None, help="Choose a batchsize, default to sweep")
     parser.add_argument("--input_dropout", "-id", type=float, default=0., help="Specify input dropout rate")
     parser.add_argument("--conv_dropout", "-cd", type=float, default=0., help="Specify conv dropout rate (applied at all layers)")
     parser.add_argument("--lstm_dropout", "-ld", type=float, default=0., help="Specify lstm dropout rate (applied to lstm output)")
     args = parser.parse_args()
 
-    # Hyper parameters
-    num_epochs = 5
-    TEST_TO_ALL_RATIO = 0.01
-    data_folder = args.input
-    nworkers = args.nthreads
-    rate_select = args.rate
-    batch_select = args.batchsize
-    dropouts = Dropouts(args.input_dropout, args.conv_dropout, args.lstm_dropout)
-
-    rates = [1e-3, 1e-9, 1e-5, 1e-7]
-    batches = [32, 64, 128, 256]
-
-    if torch.cuda.is_available():
-        torch.set_default_tensor_type('torch.cuda.FloatTensor')
-    if rate_select > 0:
-        rates = [rate_select]
-    if batch_select > 0:
-        batches = [batch_select]
+    rates = [1e-3, 1e-9, 1e-5, 1e-7] if not args.rate else args.rate
+    batches = [32, 64, 128, 256] if not args.batchsize else args.batchsize
     for learning_rate in rates:
         for batch_size in batches:
             # dir to store the experiment files
             results_dir = "results/results" + time.strftime("_%Y_%m_%d_%H_%M_%S") + '_lr{}'.format(learning_rate) + '_bs{}'.format(batch_size)
             print('writing results to {}'.format(results_dir))
-            diffraction_train(num_epochs, batch_size, learning_rate, dropouts, TEST_TO_ALL_RATIO, data_folder, results_dir, nthreads=nworkers)
+
+            config = TrainConfig() \
+                        .set_data_folder(args.input) \
+                        .set_num_threads(args.nthreads) \
+                        .set_learning_rate(learning_rate) \
+                        .set_batch_size(batch_size) \
+                        .set_dropouts(Dropouts(args.input_dropout, args.conv_dropout, args.lstm_dropout)) \
+                        .set_num_epochs(5) \
+                        .set_test_to_all_ratio(0.01) \
+                        .set_results_dir(results_dir)
+            diffraction_train(config)
